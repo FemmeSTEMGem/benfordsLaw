eb1ce41cdabbf4e45b336a1abbac5b6f8da7ee8a
femmestemgem <femmestemgem@gmail.com>
2023-07-03 18:10:19 -0400

    Adjusting notes in server.js, deleting models folder.

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // NOTES FOR RE-LEARNING FULL-STACK APPLICATIONS
 
-// We use the app.set() method to configure the view engine as EJS ('ejs') and set the views directory to the appropriate path (path.join(__dirname, 'app', 'views')).
-
-// With this setup, when you render the results.ejs file in your controller using res.render('results', { /* data */ }), Express.js will look for the results.ejs file within the specified views directory and render it with the provided data before sending it as the response to the client.
+// With this setup, when you render the results.ejs file in your controller using res.render('results', { /* data */ }),
+  // --> Express.js will look for the results.ejs file within the specified views directory
+  // --> then render it with the provided data
+  // --> then sends it as the response to the client.
 
 
 const express = require('express');
 const app = express();
 
+// We use the app.set() method to configure the view engine as EJS ('ejs') and set the views directory to the appropriate path (path.join(__dirname, 'app', 'views')).
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'app', 'views'));
 
