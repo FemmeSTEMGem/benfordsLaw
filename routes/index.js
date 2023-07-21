3c5cb3e32e5e8cad8b6ae7913815e4ea45b30d68
femmestemgem <femmestemgem@gmail.com>
2023-07-21 15:05:46 -0400

    Adding GET and POST for results.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* GET */
+
+// Route to display the home page
+router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+// Route to display the result on a new page
+router.get('/result', (req, res) => {
+  const result = req.query.output; // Get the result from the query parameter
+
+  // Render the result view with the data
+  res.render('result', { result });
+});
+
+
+/* POST */
+
+// Handling user submission
+router.post('/process', (req, res) => {
+  const userInput = req.body.userInput; // Get the input from the request body
+
+  // Call your function to process the user input
+  const result = lawCalculator(userInput);
+
+  // Redirect to the new page with the result
+  res.redirect(`/result?output=${encodeURIComponent(result)}`);
 });
 
 module.exports = router;
