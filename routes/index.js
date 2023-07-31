0741d5d22adaec3c0a93bc04c554180c00c8c6f0
femmestemgem <femmestemgem@gmail.com>
2023-07-31 15:51:01 -0400

    Uncommenting out files to use routes folder again

--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,33 @@
-// //! Initial attempt to delete routes folder resulted in an error message upon loading the main page. Use ChatGPT to determine the cause. Obviously a routing issue, just gotta pinpoint it. Also worth noting, in an attempt to isolate the problem myself first, I commented out a few pre-existing lines of code in app.js. I doubt very much that they'll need to be used, but worth mentioning just in case it's relevant later.
+var express = require('express');
+var router = express.Router();
 
-// var express = require('express');
-// var router = express.Router();
+/* GET */
 
-// /* GET */
+// Route to display the home page
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
 
-// // Route to display the home page
-// router.get('/', (req, res, next) => {
-//   res.render('index');
-// });
+// Route to display the result on a new page
+router.get('/result', (req, res) => {
+  const result = req.query.output; // Get the result from the query parameter
 
-// // Route to display the result on a new page
-// router.get('/result', (req, res) => {
-//   const result = req.query.output; // Get the result from the query parameter
-
-//   // Render the result view with the data
-//   res.render('result', { result });
-// });
+  // Render the result view with the data
+  res.render('result', { result });
+});
 
 
-// /* POST */
+/* POST */
 
-// // Handling user submission
-// router.post('/process', (req, res) => {
-//   const userInput = req.body.userInput; // Get the input from the request body
+// Handling user submission
+router.post('/process', (req, res) => {
+  const userInput = req.body.userInput; // Get the input from the request body
 
-//   // Call your function to process the user input
-//   const result = lawCalculator(userInput);
+  // Call your function to process the user input
+  const result = lawCalculator(userInput);
 
-//   // Redirect to the new page with the result
-//   res.redirect(`/result?output=${encodeURIComponent(result)}`);
-// });
+  // Redirect to the new page with the result
+  res.redirect(`/result?output=${encodeURIComponent(result)}`);
+});
 
-// module.exports = router;
+module.exports = router;
